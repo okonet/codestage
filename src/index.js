9ab44132a956f5983d63e520af57272589408368
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-04-23 20:17:13 +0200

    Enable HMR for CRA app (WIP)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './ui/App';
-import './ui/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './ui/App'
+import './ui/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(Component) {
+  ReactDOM.render(<Component />, document.getElementById('root'))
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./ui/App', () => {
+    const NextApp = require('./ui/App').default // eslint-disable-line
+    render(NextApp)
+  })
+}
