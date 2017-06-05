3d3e7e8f4c1ebed80bd85a4fb9fafa8bbfeef2d4
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-06-05 09:57:22 +0200

    Fix layout of the Main window

--- src/ui/Main.js
+++ src/ui/Main.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import LangChooser from './LangChooser'
+
+const TransparentWindow = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`
 
 export default class Main extends Component {
   render() {
-    return <LangChooser {...this.props} />
+    return (
+      <TransparentWindow>
+        <LangChooser {...this.props} />
+      </TransparentWindow>
+    )
   }
 }
