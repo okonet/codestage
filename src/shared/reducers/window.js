ece41a6f78f24845bf92b2c5e1601bab92c6d5a5
Andrey Okonetchnikov <andrey@okonet.ru>
2017-07-10 17:08:44 +0200

    Main window should stay hidden initially

--- src/shared/reducers/window.js
+++ src/shared/reducers/window.js
@@ -2,7 +2,7 @@ import { SET_MAIN_WINDOW_VISIBILITY, SET_WINDOW_SIZE } from '../actions/window'
 import { WindowSizes } from '../contants/window'
 
 const initialState = {
-  windowVisible: true,
+  windowVisible: false,
   size: WindowSizes.MINI
 }
 
