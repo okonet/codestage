5ebe5191cfb3f668e32f90a50014af7e16962947
Andrey Okonetchnikov <andrey@okonet.ru>
2017-09-15 16:27:10 +0200

    WIP on better keyboard navigaiton with arrow keys

--- src/ui/ItemsList.js
+++ src/ui/ItemsList.js
@@ -6,8 +6,11 @@ import { ListViewFooter, TextInput } from 'react-desktop'
 export default class ItemsList extends Component {
   constructor(props) {
     super()
+    const { items, selectedItem } = props
+    const selectedIndex = items.indexOf(selectedItem)
     this.state = {
       items: props.items,
+      selectedIndex,
       query: ''
     }
   }
@@ -33,14 +36,13 @@ export default class ItemsList extends Component {
   }
 
   rowRenderer = ({ key, index, style }) => {
-    const { selectedItem } = this.props
-    const { items } = this.state
+    const { items, selectedIndex } = this.state
     const item = items[index]
     return (
       <ListViewRow
         key={key}
-        onClick={() => this.onSelectionChange(item)} // eslint-disable-line react/jsx-no-bind
-        background={selectedItem === item ? '#d8dadc' : null}
+        onClick={() => this.onSelectionChange(index)} // eslint-disable-line react/jsx-no-bind
+        background={selectedIndex === index ? '#d8dadc' : null}
         style={{
           ...style,
           margin: 0,
@@ -54,11 +56,20 @@ export default class ItemsList extends Component {
     )
   }
 
-  onSelectionChange = item => {
+  onSelectionChange = index => {
     const { onSelect } = this.props
-    if (typeof onSelect === 'function') {
-      onSelect(item)
-    }
+    const { items } = this.state
+    const item = items[index]
+    this.setState(
+      {
+        selectedIndex: index
+      },
+      () => {
+        if (typeof onSelect === 'function') {
+          onSelect(item)
+        }
+      }
+    )
   }
 
   onStepperChanged = ({ scrollToRow }) => {
@@ -77,10 +88,30 @@ export default class ItemsList extends Component {
     })
   }
 
+  onKeyDown = evt => {
+    console.log(this.state)
+    switch (evt.key) {
+      case 'ArrowUp': {
+        this.setState(({ selectedIndex }) => ({
+          selectedIndex: Math.max(0, selectedIndex - 1)
+        }))
+        break
+      }
+      case 'ArrowDown': {
+        this.setState(({ items, selectedIndex }) => ({
+          selectedIndex: Math.min(selectedIndex + 1, items.length - 1)
+        }))
+        break
+      }
+      default: {
+        break
+      }
+    }
+  }
+
   render() {
-    const { heading, selectedItem } = this.props
-    const { query, items } = this.state
-    const selectedIndex = items.indexOf(selectedItem)
+    const { heading } = this.props
+    const { query, items, selectedIndex } = this.state
     return (
       <ListView>
         <ListViewHeader>
@@ -110,7 +141,7 @@ export default class ItemsList extends Component {
             </ArrowKeyStepper>}
         </AutoSizer>
         <ListViewFooter>
-          <TextInput value={query} onChange={this.onFilterChange} />
+          <TextInput value={query} onChange={this.onFilterChange} onKeyDown={this.onKeyDown} />
         </ListViewFooter>
       </ListView>
     )
