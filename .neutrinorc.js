976d71095567031cfb2a43f1cdebd6b523a9759f
Andrey Okonetchnikov <andrey@okonet.ru>
2017-10-22 12:57:30 +0200

    Use optimized for electron neutrinorc

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,18 @@
 module.exports = {
-  "use": [
-    "neutrino-preset-react",
+  use: [
+    ["neutrino-preset-react", {
+      babel: {
+        presets: [
+          ['babel-preset-env', {
+            targets: {
+              browsers: [
+                'Chrome 59',
+              ]
+            }
+          }]
+        ]
+      }
+    }],
     (neutrino) => neutrino.config.target('electron-renderer')
   ]
 }
