da12c131f14dd00ef25fc7d5963ff5d7b78ec89a
Andrey Okonetchnikov <andrey@okonet.ru>
2017-12-08 10:25:32 +0100

    Implement SearchField using react-desktop

--- src/ui/SearchField.js
+++ src/ui/SearchField.js
@@ -1,17 +1,48 @@
 import React, { PropTypes } from 'react'
-import focusOnKeyDown from 'react-focus-onkeydown'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import { SearchField as SearchInput } from 'react-desktop/macOs'
 
-const Input = styled(focusOnKeyDown('input'))`
-font-size: 16px;
+const Input = styled(SearchInput)`
+  font-size: 16px;
   width: 100%;
 `
 
-const SearchField = ({ focusable = false, ...rest }) =>
-  <Input type="search" focusOnKeyDown={focusable} {...rest} />
+class SearchField extends React.Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeydown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeydown)
+  }
+
+  handleKeydown = evt => {
+    const { focusable } = this.props
+    // Focus on Cmd + F
+    if (evt.metaKey && evt.code === 'KeyF' && this.node && focusable) {
+      const el = ReactDOM.findDOMNode(this.node) // eslint-disable-line react/no-find-dom-node
+      el.querySelector('input').focus()
+    }
+  }
+
+  setRef = node => {
+    this.node = node
+  }
+
+  render() {
+    const { ...rest } = this.props
+    delete rest.focusable
+    return <Input type="search" innerRef={this.setRef} {...rest} />
+  }
+}
 
 SearchField.propTypes = {
   focusable: PropTypes.bool
+}
+
+SearchField.defsultProps = {
+  focusable: false
 }
 
 export default SearchField
